a349c4254aec33ceed88e4aaf9bf7f021a534fca
nebisin <salimbozok@outlook.com>
2021-12-19 02:33:54 +0300

    add password reset page

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,7 +1,8 @@
 const App = {
   data() {
     return {
-      message: "test",
+      password: "",
+      isLoading: false,
     };
   },
   methods: {
@@ -11,7 +12,35 @@ const App = {
         results = regex.exec(url);
       if (!results) return null;
       if (!results[2]) return "";
-      return decodeURIComponent(results[2].replace(/\+/g, " "));
+      return decodeURIComponent(results[2]);
+    },
+    onSubmit() {
+      this.isLoading = true;
+      const token = this.getParameterByName("token");
+      fetch("/api/v1/users/password", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          password: this.password,
+          token: token,
+        }),
+      })
+        .then((response) => {
+          if (response.status === 200) {
+            alert("Password changed successfully");
+          } else {
+            alert("Something went wrong");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Something went wrong");
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
     },
   },
 };
